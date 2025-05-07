ea6afe4d2e41ba658a09869da910130bd8a45c5e
reza farazi <rezafarazi1378@gmail.com>
2025-05-07 09:35:42 +0330

    Get start

---
+++ src/main/java/com/cyberdyne/skynet/client/App.java
@@ -0,0 +1,15 @@
+package com.cyberdyne.skynet.client;
+
+import com.cyberdyne.skynet.client.Services.Config.Config;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        //Get config from file
+        new Config();
+
+
+
+    }
+}
